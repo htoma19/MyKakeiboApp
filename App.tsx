a2f638b9e779569f8a123be70bc31bd0c6b850ae
toma <touma0345@gmail.com>
2025-12-05 21:24:41 +0900

    First commit: 環境構築完了とドキュメント作成

--- App.tsx
+++ App.tsx
@@ -1,45 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// ----- ここからコピー -----
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+function App(): React.JSX.Element {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.center}>
+        <Text style={styles.title}>
+          家計簿アプリ
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, // 画面全体を使う
+  },
+  center: {
+    flex: 1, // 画面全体を使う
+    justifyContent: 'center', // 縦（垂直）方向の中央揃え
+    alignItems: 'center', // 横（水平）方向の中央揃え
+  },
+  title: {
+    fontSize: 24, // 文字サイズ
+    fontWeight: 'bold', // 太字
   },
 });
 
 export default App;
+// ----- ここまでコピー -----
